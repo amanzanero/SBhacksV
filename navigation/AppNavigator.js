27dd4fc3971a90e79182a4c181687698848c0605
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-01-12 07:12:16 -0800

    Change Profile to credentials

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,7 +3,7 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import LoginScreen from '../screens/LoginScreen';
-import Profile from '../screens/Profile';
+import Credentials from '../screens/Credentials';
 import Preferences from '../screens/Preferences';
 
 
@@ -13,7 +13,7 @@ export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: LoginScreen,
-  Creds: Profile,
+  Creds: Credentials,
   Prefs: Preferences,
   AppContents: MainTabNavigator,
 }));
